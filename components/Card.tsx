7092934664d5b3e2677babb2a0cdebc49abc802e
Chayanin Kongsareekul <57552210+Aphrodicez@users.noreply.github.com>
2024-11-19 20:05:17 +0700

    Feat: Add Neos components

---
+++ components/Card.tsx
@@ -0,0 +1,39 @@
+'use client'
+import Image from "next/image"
+import InteractionCard from "./InteractiveCard"
+import { Rating } from '@mui/material';
+import { useState } from 'react';
+
+interface Props {
+    imgSrc: string;
+    companyName: string;
+    onRating?: Function;
+}
+
+export default function Card({imgSrc, companyName, onRating} : Props) {
+    const [rating, setRating] = useState<number | null>(0);
+    return (
+        <InteractionCard contentName={companyName}>
+            <div className="img w-[100%] h-[70%] relative object-cover">
+                <Image src={imgSrc}
+                    alt='cover'
+                    fill={true} />
+            </div>
+            <div className="txt p-[10px]">
+                {companyName}
+            </div>
+            <Rating size = "medium"
+                    id = {companyName + " " + "Rating"}
+                    name = {companyName + " " + "Rating"}
+                    data-testid = {companyName + " " + "Rating"}
+                    value = {rating}
+                    onChange={(event, newRating) => {
+                        setRating(newRating);
+                        if(onRating) {
+                            onRating(companyName, newRating);
+                        }
+                    }}
+            />
+        </InteractionCard>
+    )
+}
---
+++ components/Searchbar.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { debounce } from '@mui/material';
+
+interface SearchBarProps {
+  className?: string;
+  basePath?: string;
+  onSearchChange?: (key: string, value: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ className, basePath, onSearchChange }) => {
+  const router = useRouter();
+  const [searchValue, setSearchValue] = useState<string>('');
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+
+  const currentPath = basePath || pathName.split('/').pop() || '';
+  const defaultValue = searchParams.get('q') || '';
+
+  const debouncedSearch = debounce((value: string) => {
+    if (value) {
+      router.push(`/${currentPath}`);
+    } else {
+      router.push(`/${currentPath}`);
+    }
+  }, 300);
+
+  const handleSearch = (value: string) => {
+    setSearchValue(value);
+    debouncedSearch(value);
+  };
+
+  return (
+    <input
+      type="text"
+      defaultValue={defaultValue}
+      className='w-full max-w-xs pr-10'
+      placeholder="Search the library"
+      onChange={(e) => handleSearch(e.target.value)}
+    />
+  );
+};
+
+export default SearchBar;
